ec37381599a1ba62cc8df534202db11c0bcfc074
Savitoj Singh <savitojsingh15@gmail.com>
2017-11-24 05:01:17 +0000

    settings.py edited online with Bitbucket

--- loop_attendance/settings.py
+++ loop_attendance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '((3g7o#2&)veqogaszj+4jfp1!bn9x@76i_tc8*8&)$!*49o@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'loopedu',
-        'USER': 'root',
-        'PASSWORD': 'savi',
+        'USER': 'loopedu',
++       'PASSWORD': 'fVcXhgQYEgjR0K3ubHLV',
         
     }
 }
